737236e109ba43b5bb8f64a7db6d71998ce82b18
Takeo Miyamoto <miyamofigo@gmail.com>
2015-08-06 19:37:57 +0900

    Indents has been organized.

--- src/lib.rs
+++ src/lib.rs
@@ -3,40 +3,40 @@
 #[test]
 fn it_works() {
 
-	let cypher = encode("helloworld", 3);
-	assert!("helloworld" == decode(cypher.as_str(), 3)); 
+  let cypher = encode("helloworld", 1);
+  assert!("helloworld" == decode(cypher.as_str(), 1)); 
 
 }
 
 fn encode(enc: &str, offset: u32) -> String {
 
-	let offset = offset % 26 + 26;
-	let encoded: String = 
-								enc.chars()
-		 							 .map( |c| { 
-										 				   let res = match c.is_alphabetic() {
+  let offset = offset % 26 + 26;
+  let encoded: String = 
+                enc.chars()
+                   .map( |c| { 
+                               let res = match c.is_alphabetic() {
 
-																					 true => { 
-																						 let base = 
-																							 if c.is_uppercase() 
-																									 { 64 } else { 96 };
-																						 let d = base + ((c as u32) 
-																													- base 
-																													+ offset) % 26;
-																						 std::char::from_u32(d).unwrap()
-																					 },
-																					 _ => c 
+                                           true => { 
+                                             let base = 
+                                               if c.is_uppercase() 
+                                                   { 64 } else { 96 };
+                                             let d = base + ((c as u32) 
+                                                          - base 
+                                                          + offset) % 26;
+                                             std::char::from_u32(d).unwrap()
+                                           },
+                                           _ => c 
 
-																			 }; 
-															 res	
-													} )
-		 							 .collect();
-	encoded
-									
+                                       }; 
+                               res  
+                          } )
+                   .collect();
+  encoded
+                  
 } 
 
 fn decode(enc: &str, offset: u32) -> String {
-		
-	encode(enc, 26 - offset)
+    
+  encode(enc, 26 - offset)
 
 }  
